63919307e94ecd254bc55cca95ab7851d0865b9c
Lauren Kessell <lkessell1@gmail.com>
2021-08-04 14:11:15 -0400

    Map through prop data to render icons

--- src/components/IconContainer/IconContainer.js
+++ src/components/IconContainer/IconContainer.js
@@ -1,9 +1,16 @@
 import './IconContainer.css';
+import VillagerIcon from '../VillagerIcon/VillagerIcon';
 
-const IconContainer = () => {
+const IconContainer = ({ allVillagers }) => {
+
+  const villagers = allVillagers.map(villager => {
+    return <VillagerIcon key={villager.id} id={villager.id} img={villager.icon_uri}/>
+  });
+
   return (
     <section className='icon-container'>
       <h2>Villager icons will be here</h2>
+      {villagers}
     </section>
   );
 }
