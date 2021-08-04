a99060eb260ced28587d337cf93d32f8be955c01
Lauren Kessell <lkessell1@gmail.com>
2021-08-04 18:05:07 -0400

    Add Router Link to details page

--- src/components/VillagerIcon/VillagerIcon.js
+++ src/components/VillagerIcon/VillagerIcon.js
@@ -1,8 +1,11 @@
 import './VillagerIcon.css';
+import { Link } from 'react-router-dom';
 
 const VillagerIcon = ({ id, img, altText }) => {
   return (
-    <img src={img} id={id} alt={altText} />
+    <Link to={`/villagers/${id}`}>
+      <img src={img} id={id} alt={altText} />
+    </Link>
   );
 }
 
