4756083edcaf9593260663ba79715c2059120a70
Lauren Kessell <lkessell1@gmail.com>
2021-08-04 22:11:09 -0400

    Add wishlist component

---
+++ src/components/Wishlist/Wishlist.js
@@ -0,0 +1,11 @@
+import './Wishlist.css';
+
+const Wishlist = () => {
+  return (
+    <section className='wishlist'>
+      <h2>I'm a wishlist</h2>
+    </section>
+  );
+}
+
+export default Wishlist;
