045de2db572638a84fb699462b212e1dece6714f
Lauren Kessell <lkessell1@gmail.com>
2021-08-04 20:33:39 -0400

    Add villager info to details page

--- src/components/VillagerDetails/VillagerDetails.js
+++ src/components/VillagerDetails/VillagerDetails.js
@@ -3,7 +3,30 @@ import './VillagerDetails.css';
 const VillagerDetails = ({ data }) => {
   return (
     <section className='villager-details'>
-      <h3>{data.name['name-USen']}</h3>
+      <img src={`https://acnhapi.com/v1/images/villagers/${data.id}`} alt=''/>
+      <h3 className='details-name'>{data.name['name-USen']}</h3>
+      <dl>
+        <div>
+          <dt>Species</dt>
+          <dd>{data.species}</dd>
+        </div>
+        <div>
+          <dt>Birthday</dt>
+          <dd>{data['birthday-string']}</dd>
+        </div>
+        <div>
+          <dt>Gender</dt>
+          <dd>{data.gender}</dd>
+        </div>
+        <div>
+          <dt>Hobby</dt>
+          <dd>{data.hobby}</dd>
+        </div>
+        <div>
+          <dt>Saying</dt>
+          <dd>{data.saying}</dd>
+        </div>
+      </dl>
     </section>
   );
 }
