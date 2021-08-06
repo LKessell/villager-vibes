69e7a02d8c212c5156731fccffa141243c4726d8
Lauren Kessell <lkessell1@gmail.com>
2021-08-06 13:23:17 -0400

    Add Nav links for home/wishlist

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -4,6 +4,8 @@ import { NavLink } from 'react-router-dom';
 const NavBar = () => {
   return (
     <nav>
+      <NavLink exact to='/' className='nav-home'>Home</NavLink>
+      <NavLink to='/wishlist' className='nav-wishlist'>Wishlist</NavLink>
     </nav>
   );
 }
