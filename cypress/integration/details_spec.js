d941e613469287fb5178bb208b6d3b3cce729096
Lauren Kessell <lkessell1@gmail.com>
2021-08-07 15:53:15 -0400

    Add tests for details, photo, and button visibility

--- cypress/integration/details_spec.js
+++ cypress/integration/details_spec.js
@@ -6,4 +6,16 @@ describe('Villager details user flows', () => {
     cy.visit('http://localhost:3000/');
       cy.get('#140').click();
   });
+
+  it('Should contain a photo and more details about the villager', () => {
+    cy.get('.detail-photo').should('be.visible');
+    cy.contains('Ketchup');
+    cy.contains('July 27th');
+    cy.contains('When in doubt, eat a tomato!');
+  });
+
+  it('Should have buttons to reject the villager, or add them to the wishlist', () => {
+    cy.get('.details-back-btn').should('be.visible');
+    cy.get('.add-wishlist-btn').should('be.visible');
+  });
 });
