be4acc25e8ca97ce2aa5ab16c7c5aea25a3c2af8
Lauren Kessell <lkessell1@gmail.com>
2021-08-07 09:49:52 -0400

    Add state to track selected filter

--- src/components/Filter/Filter.js
+++ src/components/Filter/Filter.js
@@ -1,15 +1,18 @@
+import { useState } from 'react';
 import './Filter.css';
 import PropTypes from 'prop-types';
 
 const Filter = ({ displayedVillagers, setDisplayedVillagers }) => {
+  const [selectedFilter, setSelectedFilter] = useState('all')
+
   return (
     <section className='filter-container'>
       <label>
-        <input type='radio' className='radio-bubble' id='all' checked={true} />
+        <input type='radio' className='radio-bubble' id='all' checked={selectedFilter === 'all'} />
       All Villagers
       </label>
       <label>
-        <input type='radio' className='radio-bubble' id='Bird' checked={false} />
+        <input type='radio' className='radio-bubble' id='Bird' checked={selectedFilter === 'Bird'} />
       Bird
       </label>
     </section>
