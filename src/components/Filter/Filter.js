1cf671d3c6b83dc5de7f4c105c5c041c5bf44516
Lauren Kessell <lkessell1@gmail.com>
2021-08-07 09:59:39 -0400

    Add onChange function to filter villagers

--- src/components/Filter/Filter.js
+++ src/components/Filter/Filter.js
@@ -2,17 +2,24 @@ import { useState } from 'react';
 import './Filter.css';
 import PropTypes from 'prop-types';
 
-const Filter = ({ displayedVillagers, setDisplayedVillagers }) => {
-  const [selectedFilter, setSelectedFilter] = useState('all')
+const Filter = ({ allVillagers, displayedVillagers, setDisplayedVillagers }) => {
+  const [selectedFilter, setSelectedFilter] = useState('all');
+
+  const filterVillagers = event => {
+    const filteredResults = allVillagers.filter(villager => villager.species === event.target.id || event.target.id === 'all');
+
+    setSelectedFilter(event.target.id);
+    setDisplayedVillagers(filteredResults);
+  }
 
   return (
     <section className='filter-container'>
       <label>
-        <input type='radio' className='radio-bubble' id='all' checked={selectedFilter === 'all'} />
+        <input type='radio' className='radio-bubble' id='all' checked={selectedFilter === 'all'} onChange={event => filterVillagers(event)} />
       All Villagers
       </label>
       <label>
-        <input type='radio' className='radio-bubble' id='Bird' checked={selectedFilter === 'Bird'} />
+        <input type='radio' className='radio-bubble' id='Bird' checked={selectedFilter === 'Bird'} onChange={event => filterVillagers(event)} />
       Bird
       </label>
     </section>
