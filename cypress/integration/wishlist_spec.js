68212051cda0063baa9f2672025c8d016424733a
Lauren Kessell <lkessell1@gmail.com>
2021-08-07 17:09:21 -0400

    Update beforeEach

--- cypress/integration/wishlist_spec.js
+++ cypress/integration/wishlist_spec.js
@@ -1,5 +1,8 @@
 describe('Wishlist user flows', () => {
-  cy.fixture('sampleVillagers').then((villagersData) => {
-    cy.intercept('https://acnhapi.com/v1a/villagers/', villagersData);
-  cy.visit('http://localhost:3000/');
+  beforeEach(() => {
+    cy.fixture('sampleVillagers').then((villagersData) => {
+      cy.intercept('https://acnhapi.com/v1a/villagers/', villagersData);
+    });
+    cy.visit('http://localhost:3000/');
+  });
 });
