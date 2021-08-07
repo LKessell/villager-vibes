5d19687e37274df61d5e97285c0846be010d4355
Lauren Kessell <lkessell1@gmail.com>
2021-08-07 09:41:56 -0400

    Add props and PropTypes

--- src/components/Filter/Filter.js
+++ src/components/Filter/Filter.js
@@ -1,6 +1,7 @@
 import './Filter.css';
+import PropTypes from 'prop-types';
 
-const Filter = () => {
+const Filter = ({ displayedVillagers, setDisplayedVillagers }) => {
   return (
     <section className='filter-container'>
       <label>
@@ -16,3 +17,8 @@ const Filter = () => {
 }
 
 export default Filter;
+
+Filter.propTypes = {
+  displayedVillagers: PropTypes.array,
+  setDisplayedVillagers: PropTypes.func
+}
