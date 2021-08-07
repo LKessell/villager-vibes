4ba5e35b1f6630856a863492745ff7b3b774cb70
Lauren Kessell <lkessell1@gmail.com>
2021-08-07 17:14:46 -0400

    Add test for adding multiple villagers to wishlist

--- cypress/integration/wishlist_spec.js
+++ cypress/integration/wishlist_spec.js
@@ -5,4 +5,16 @@ describe('Wishlist user flows', () => {
     });
     cy.visit('http://localhost:3000/');
   });
+
+  it('Should be able to add multiple villagers to the wishlist', () => {
+    cy.get('#155').click();
+    cy.get('.add-wishlist-btn').click();
+    cy.get('.nav-home').click();
+    cy.get('#140').click();
+    cy.get('.add-wishlist-btn').click();
+    cy.contains('The Top 10');
+    cy.get('#155').should('be.visible');
+    cy.get('#140').should('be.visible');
+    cy.get('#384').should('not.exist');
+  });
 });
