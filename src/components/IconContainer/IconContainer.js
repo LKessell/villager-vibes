106647835054319f925a36ff887e30854b549c8a
Lauren Kessell <lkessell1@gmail.com>
2021-08-08 12:07:04 -0400

    Add new type prop

--- src/components/IconContainer/IconContainer.js
+++ src/components/IconContainer/IconContainer.js
@@ -2,12 +2,12 @@ import './IconContainer.css';
 import PropTypes from 'prop-types';
 import VillagerIcon from '../VillagerIcon/VillagerIcon';
 
-const IconContainer = ({ villagers }) => {
+const IconContainer = ({ type, villagers }) => {
 
   const currentVillagers = villagers.map(villager => {
     return <VillagerIcon key={villager.id} id={villager.id} img={villager.icon_uri} altText={villager.name['name-USen']}/>
   });
-  const loadingMsg = !villagers.length && <h2>Loading villagers...</h2>;
+  const loadingMsg = !villagers.length && type === 'main' && <h2>Loading villagers...</h2>;
 
   return (
     <section className='icon-container'>
@@ -20,5 +20,6 @@ const IconContainer = ({ villagers }) => {
 export default IconContainer;
 
 IconContainer.propTypes = {
+  type: PropTypes.string,
   villagers: PropTypes.array
 };
