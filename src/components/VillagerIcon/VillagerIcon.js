582cf9eddfaa67562d36cde5827ddafc0a8ba095
Lauren Kessell <lkessell1@gmail.com>
2021-08-08 15:46:47 -0400

    Add remove icon to villagers on wishlist

--- src/components/VillagerIcon/VillagerIcon.js
+++ src/components/VillagerIcon/VillagerIcon.js
@@ -1,11 +1,15 @@
 import './VillagerIcon.css';
 import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
+import circleX from '../../remove.png';
 
-const VillagerIcon = ({ id, img, altText }) => {
+const VillagerIcon = ({ id, img, altText, type }) => {
+  const removeBtn = type === 'wishlist' && <img src={circleX} alt='remove' className='remove-btn'/>
+
   return (
     <Link to={`/villagers/${id}`}>
       <img src={img} id={id} alt={altText} />
+      {removeBtn}
     </Link>
   );
 }
@@ -15,5 +19,6 @@ export default VillagerIcon;
 VillagerIcon.propTypes = {
   id: PropTypes.number,
   img: PropTypes.string,
-  altText: PropTypes.string
+  altText: PropTypes.string,
+  type: PropTypes.string,
 }
