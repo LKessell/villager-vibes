98e4704fc7765a2b2da5cd48636fbf1b587c8b04
Lauren Kessell <lkessell1@gmail.com>
2022-07-21 16:17:14 -0400

    Adjust markup, show remove button on keyboard focus

--- src/components/VillagerIcon/VillagerIcon.js
+++ src/components/VillagerIcon/VillagerIcon.js
@@ -3,27 +3,30 @@ import { Link } from "react-router-dom";
 import PropTypes from "prop-types";
 
 const VillagerIcon = ({ id, img, altText, type, setWishlist, villagers }) => {
-  const removeVillager = (id) => {
-    const splitID = id.split("-");
+  const removeVillager = (e) => {
+    e.preventDefault();
+
+    const splitID = e.target.id.split("-");
     const numID = parseInt(splitID[1]);
     const newWishlist = villagers.filter((villager) => villager.id !== numID);
+
     setWishlist(newWishlist);
   };
 
   return (
-    <div className="icon-wrapper">
-      <Link to={`/villagers/${id}`}>
+    <Link to={`/villagers/${id}`} className="icon-link">
+      <div>
         <img src={img} id={id} alt={altText} className="villager-icon" />
-      </Link>
-      {type === "wishlist" && (
-        <button
-          aria-label="Remove from wishlist"
-          className="remove-btn"
-          id={`rem-${id}`}
-          onClick={(event) => removeVillager(event.target.id)}
-        />
-      )}
-    </div>
+        {type === "wishlist" && (
+          <button
+            aria-label="Remove from wishlist"
+            className="remove-btn"
+            id={`rem-${id}`}
+            onClick={(e) => removeVillager(e)}
+          />
+        )}
+      </div>
+    </Link>
   );
 };
 
