3a38ca954a0a7091202fd737f93f1395a116a4b8
Lauren Kessell <lkessell1@gmail.com>
2022-08-27 12:56:51 -0400

    Update existing wishlist tests

--- cypress/e2e/wishlist.cy.js
+++ cypress/e2e/wishlist.cy.js
@@ -1,64 +1,68 @@
-describe('Wishlist user flows', () => {
+describe("Wishlist user flows", () => {
   beforeEach(() => {
-    cy.fixture('sampleVillagers').then((villagersData) => {
-      cy.intercept('https://acnhapi.com/v1a/villagers/', villagersData);
+    cy.fixture("sampleVillagers").then((villagersData) => {
+      cy.intercept("https://acnhapi.com/v1a/villagers/", villagersData);
     });
-    cy.visit('http://localhost:3000/');
+    cy.visit("http://localhost:3000/");
   });
 
-  it('Should show a message if the wishlist is empty', () => {
-    cy.get('.nav-wishlist').click();
-    cy.contains('You haven\'t added any villagers yet!');
+  it("Should show a message if the wishlist is empty", () => {
+    cy.get(".nav-wishlist").click();
+    cy.contains("You haven't added any villagers yet!");
   });
 
-  it('Should be able to add multiple villagers to the wishlist', () => {
-    cy.get('#155').click();
-    cy.get('.add-wishlist-btn').click();
-    cy.get('.nav-home').click();
-    cy.get('#140').click();
-    cy.get('.add-wishlist-btn').click();
-    cy.contains('The Top 10');
-    cy.get('#155').should('be.visible');
-    cy.get('#140').should('be.visible');
-    cy.get('#384').should('not.exist');
+  it("Should be able to add multiple villagers to the wishlist", () => {
+    cy.get("#155").click();
+    cy.get(".add-wishlist-btn").click();
+    cy.get(".nav-home").click();
+    cy.get("#140").click();
+    cy.get(".add-wishlist-btn").click();
+    cy.contains("The Top 10");
+    cy.get("#155").should("be.visible");
+    cy.get("#140").should("be.visible");
+    cy.get("#384").should("not.exist");
   });
 
-  it('Cannot add duplicate villagers', () => {
-    cy.get('#155').click();
-    cy.get('.add-wishlist-btn').click();
-    cy.get('#155').should('be.visible');
-    cy.get('.nav-home').click();
-    cy.get('#155').click();
-    cy.get('.add-wishlist-btn').click();
-    cy.get('img').should('have.length', 2);
+  it("Cannot add duplicate villagers", () => {
+    cy.get("#155").click();
+    cy.get(".add-wishlist-btn").click();
+    cy.get("#155").should("be.visible");
+    cy.get(".nav-home").click();
+    cy.get("#155").click();
+    cy.get(".add-wishlist-btn").should("have.class", "add-disabled");
+    cy.get("p").contains("already on your wishlist");
   });
 
-  it('Wishlist nav link is active', () => {
-    cy.get('#155').click();
-    cy.get('.add-wishlist-btn').click();
-    cy.get('.nav-wishlist').should('have.class', 'active');
+  it("Wishlist nav link is active", () => {
+    cy.get("#155").click();
+    cy.get(".add-wishlist-btn").click();
+    cy.get(".nav-wishlist").should("have.class", "active");
   });
 
-  it('Wishlist nav link is functional', () => {
-    cy.get('.wishlist-title').should('not.exist');
-    cy.get('.nav-wishlist').click();
-    cy.get('.wishlist-title').should('be.visible');
+  it("Wishlist nav link is functional", () => {
+    cy.get(".wishlist-title").should("not.exist");
+    cy.get(".nav-wishlist").click();
+    cy.get(".wishlist-title").should("be.visible");
   });
 
-  it('Should have a remove button for villagers on the wishlist', () => {
-    cy.get('#140').click();
-    cy.get('.add-wishlist-btn').click();
-    cy.get('#rem-140').should('be.visible');
-  });
+  // Note: Cypress currently does not support CSS :hover states
+  // The remove button is only visible when hovering over a villager icon
+  // https://github.com/cypress-io/cypress/issues/3222
 
-  it('Can remove a villager from the wishlist by clicking the remove button', () => {
-    cy.get('#155').click();
-    cy.get('.add-wishlist-btn').click();
-    cy.get('.nav-home').click();
-    cy.get('#140').click();
-    cy.get('.add-wishlist-btn').click();
-    cy.get('#155').should('be.visible');
-    cy.get('#rem-155').click();
-    cy.get('#155').should('not.exist');
+  // it("Should have a remove button for villagers on the wishlist", () => {
+  //   cy.get("#140").click();
+  //   cy.get(".add-wishlist-btn").click();
+  //   cy.get("#rem-140").should("be.visible");
+  // });
+
+  it("Can remove a villager from the wishlist by clicking the remove button", () => {
+    cy.get("#155").click();
+    cy.get(".add-wishlist-btn").click();
+    cy.get(".nav-home").click();
+    cy.get("#140").click();
+    cy.get(".add-wishlist-btn").click();
+    cy.get("#155").should("be.visible");
+    cy.get("#rem-155").click({ force: true });
+    cy.get("#155").should("not.exist");
   });
 });
