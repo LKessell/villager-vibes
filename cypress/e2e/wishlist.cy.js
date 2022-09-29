4147cf9ec47a79641766288600532e12a68c338b
Lauren Kessell <lkessell1@gmail.com>
2022-09-29 15:44:12 -0400

    Add Cypress test for localStorage

--- cypress/e2e/wishlist.cy.js
+++ cypress/e2e/wishlist.cy.js
@@ -33,6 +33,18 @@ describe("Wishlist user flows", () => {
     cy.get("p").contains("already on your wishlist");
   });
 
+  it("Should save the current wishlist to localStorage", () => {
+    expect(localStorage.getItem("villager-wishlist")).to.equal("[]");
+    cy.get("#384").click();
+    cy.get(".add-wishlist-btn")
+      .click()
+      .then(() => {
+        expect(localStorage.getItem("villager-wishlist")).to.equal(
+          '[{"id":384,"file-name":"wol03","name":{"name-USen":"Whitney","name-EUen":"Whitney","name-EUde":"Lupa","name-EUes":"Lupe","name-USes":"Lupe","name-EUfr":"Blanche","name-USfr":"Blanche","name-EUit":"Bianca","name-EUnl":"Whitney","name-CNzh":"毕安卡","name-TWzh":"畢安卡","name-JPja":"ビアンカ","name-KRko":"비앙카","name-EUru":"Уитни"},"personality":"Snooty","birthday-string":"September 17th","birthday":"17/9","species":"Wolf","gender":"Female","subtype":"B","hobby":"Fashion","catch-phrase":"snappy","icon_uri":"https://acnhapi.com/v1/icons/villagers/384","image_uri":"https://acnhapi.com/v1/images/villagers/384","bubble-color":"#bff2ff","text-color":"#85a2af","saying":"Don\'t cry wolf unless you mean it.","catch-translations":{"catch-USen":"snappy","catch-EUen":"snappy","catch-EUde":"jaulll","catch-EUes":"auf-auf","catch-USes":"auf-auf","catch-EUfr":"sbam","catch-USfr":"bing bang","catch-EUit":"snappy","catch-EUnl":"hap","catch-CNzh":"漂亮","catch-TWzh":"漂亮","catch-JPja":"ステキね","catch-KRko":"멋져","catch-EUru":"цап"}}]'
+        );
+      });
+  });
+
   it("Wishlist nav link is active", () => {
     cy.get("#155").click();
     cy.get(".add-wishlist-btn").click();
