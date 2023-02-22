372fb8dc33323daa5b2cdb7447ef2ee13d745121
Lauren Kessell <lkessell1@gmail.com>
2023-02-22 13:56:50 -0500

    Add file for TS Interfaces, add Villager

---
+++ interfaces.d.ts
@@ -0,0 +1,12 @@
+interface Villager {
+  'birthday-string': string;
+  'bubble-color': string;
+  gender: string;
+  hobby: string;
+  id: number;
+  icon_uri: string;
+  image_uri: string;
+  name: { 'name-USen': string };
+  personality: string;
+  species: string;
+}
