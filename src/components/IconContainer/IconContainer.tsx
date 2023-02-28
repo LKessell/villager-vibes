fb236544f7b252bdfc92acfc36484080ef15fcd7
Lauren Kessell <lkessell1@gmail.com>
2023-02-27 19:26:41 -0500

    Rename file, remove PropTypes

--- src/components/IconContainer/IconContainer.tsx
+++ src/components/IconContainer/IconContainer.tsx
@@ -1,10 +1,8 @@
 import './IconContainer.css';
-import PropTypes from 'prop-types';
 import VillagerIcon from '../VillagerIcon/VillagerIcon';
 
 const IconContainer = ({ type, villagers, setWishlist }) => {
-
-  const currentVillagers = villagers.map(villager => {
+  const currentVillagers = villagers.map((villager) => {
     return (
       <VillagerIcon
         key={villager.id}
@@ -23,18 +21,12 @@ const IconContainer = ({ type, villagers, setWishlist }) => {
   const wishlistMsg = !villagers.length && type === 'wishlist' && <h2>You haven't added any villagers yet!</h2>;
 
   return (
-    <section className='icon-container box'>
+    <section className="icon-container box">
       {loadingMsg}
       {wishlistMsg}
       {currentVillagers}
     </section>
   );
-}
+};
 
 export default IconContainer;
-
-IconContainer.propTypes = {
-  type: PropTypes.string,
-  villagers: PropTypes.array,
-  setWishlist: PropTypes.func
-};
